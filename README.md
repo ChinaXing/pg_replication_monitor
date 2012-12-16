c6d15128ffa8253070922e809c0f15fa4cfc99a2
ChinaXing <chen.yack@gmail.com>
2012-12-16 20:26:23 +0800

    ---complete first release---

--- README.md
+++ README.md
@@ -2,3 +2,26 @@ pg_replication_monitor
 ======================
 
 postgresql replication monitor
+
+I use four variable in postgresql to judgment the health of replication:
+
+- **sent_location** (in master) 
+
+- **pg_current_xlog_location()** (in master)
+
+- **pg_last_xlog_receive_location()** (in slave)
+
+- **pg_last_xlog_replay_location()** (in slave)
+
+
+to produce three indicators:
+
+- **send_receive_lag** : indicate the send-receive delay between master and slave
+
+- **current_sent_lag** : indicate the write-send delay of master
+
+- **replay_receive_lag** : indicate the receive-replay delay of slave
+
+You can set Warn and Critical threshold in configuration file as needed
+
+You can also specify a reporter program to do the report, such as SMS message sender.
--- pg_replication_monitor.sh
+++ pg_replication_monitor.sh
@@ -1,4 +1,4 @@
-#!/bin/env bash 
+#!/usr/bin/env bash 
 #set -x
 ############################################################
 #Author    : ChinaXing - chen.yack@gmail.com
@@ -6,11 +6,13 @@
 #Function  : monitor pgsql's replication
 ############################################################
 
-source repl_mon.conf
-
 function error_report
 {
     echo "[$1]$2"
+    if [ -n "$REPORTER" ]
+    then
+        $REPORTER "$@"
+    fi
     exit 1
 }
 
@@ -34,6 +36,21 @@ function get_status
     pg_last_xlog_receive_location=${pg_last_xlog_receive_location##*/}
     pg_last_xlog_replay_location=${pg_last_xlog_replay_location##*/}
 
+    if  [ -z "$sent_location" ] || \
+        [ -z "$pg_last_xlog_receive_location" ] || \
+        [ -z "$pg_last_xlog_replay_location" ] || \
+        [ -z "$pg_current_xlog_location" ]
+    then
+        error_report "ERROR" "status has NULL value:
+        sent_location:$sent_location
+        pg_current_xlog_location:$pg_current_xlog_location
+        pg_last_xlog_receive_location:$pg_last_xlog_receive_location
+        pg_last_xlog_replay_location:$pg_last_xlog_replay_location
+        "
+        return 1
+    fi
+
+    return 0
 }
 
 function check
@@ -42,25 +59,88 @@ function check
   let current_sent_lag=16#$pg_current_xlog_location-16#$sent_location
   let replay_receive_lag=16#$pg_last_xlog_replay_location-16#$pg_last_xlog_receive_location
 
-  echo send_receive_lag:$sent_receive_lag
-  echo current_sent_lag:$current_sent_lag 
-  echo replay_receive_lag:$replay_receive_lag
+  if [ -n "$DEBUG" ]
+  then
+      echo send_receive_lag:$sent_receive_lag
+      echo current_sent_lag:$current_sent_lag 
+      echo replay_receive_lag:$replay_receive_lag
+  fi
 }
 
+function report
+{
+    if [ $send_receive_lag -gt $SEND_RECEIVE_LAG_MAX ]
+    then
+        error_report "WARN" "SEND_RECEIVE_LAG -gt $SEND_RECEIVE_LAG_MAX : $send_receive_lag"
+    fi
+
+    if [ $current_sent_lag -gt $CURRENT_SENT_LAG_MAX ]
+    then
+        error_report "WARN" "CURRENT_SENT_LAG -gt $CURRENT_SENT_LAG_MAX : $current_sent_lag"
+    fi
+
+    if [ $replay_receive_lag -gt $REPLAY_RECEIVE_LAG_MAX ]
+    then
+        error_report "WARN" "REPLAY_RECEIVE_LAG -g $REPLAY_RECEIVE_LAG_MAX : $replay_receive_lag"
+    fi
+}
+
+function usage
+{
+    echo """
+    $0 -- monitor postgresql's replication
+
+
+    --OPTIONS--
+    -h         this message
+    -D         do debug, show debug info out
+    -c FILE    indicate the configuration file FILE, default is repl_mon.conf in current directory.
+    -l FILE    write log to FILE, default is STDOUT
+
+
+    eg:
+          $0 -c my_replication_mon.conf -l ./replication_monitor.log
+    """
+}
+
+function parse_args
+{
+    while getopts "hDc:l:" option
+    do
+        case $option in 
+            h) usage
+                exit 0
+                ;;
+            D) DEBUG=TRUE
+                ;;
+            c) CONF=$OPTARG
+                ;;
+            l) LOG=$OPTARG
+                ;;
+            *) error_report "ERROR" "Invalid argument : $option"
+                exit 1
+                ;;
+        esac
+    done
+
+    if [ -n "$CONF" ]
+    then
+        source $CONF
+    else
+        source repl_mon.conf
+    fi
+
+    if [ -n "$LOG" ]
+    then
+        exec > $LOG
+        exec 2 > $LOG
+    fi
+}
+
+
+# --------------- Main -------------- #
+parse_args "$@"
 get_status
-
-if  [ -z "$sent_location" ] || \
-    [ -z "$pg_last_xlog_receive_location" ] || \
-    [ -z "$pg_last_xlog_replay_location" ] || \
-    [ -z "$pg_current_xlog_location" ]
-then
-    error_report "ERROR" "status has NULL value:
-    sent_location:$sent_location
-    pg_current_xlog_location:$pg_current_xlog_location
-    pg_last_xlog_receive_location:$pg_last_xlog_receive_location
-    pg_last_xlog_replay_location:$pg_last_xlog_replay_location
-    "
-fi
-
-
 check
+report
+# --------------- END --------------- #
